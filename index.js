9fce737a2d13815c22b53ba342d4ec1e7ca83a3f
huyan <huyan00@hotmail.com>
2017-12-31 21:29:11 +0800

    project init

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const Koa = require('koa');
-
+const Router = require('koa-router');
+const views = require('koa-views');
+const koaStatic = require('koa-static');
+const ejs = require('ejs');
+const path = require('path');
+const bodyParser = require('koa-bodyparser');
+const router = require('./route');
+const session = require('koa-generic-session');
+const flash = require('koa-better-flash');
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = 'hello world'
+// 配置session
+app.use(session({
+  key: 'SC_SID',
+  httpOnly: true,
+  maxAge: 24 * 60 * 60 * 1000,
+  overwrite: true,
+}));
+
+// 配置flash
+app.use(flash());
+
+// 配置静态资源目录
+app.use(koaStatic(
+  path.join(__dirname, './public')
+));
+
+// 配置全局变量
+app.use(async (ctx, next) => {
+  ctx.state.success = ctx.flash('success').toString();
+  ctx.state.error = ctx.flash('error').toString();
 });
+
+// 配置模板引擎
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}));
+
+// 配置表单解析
+app.use(bodyParser());
+
+// 配置路由
+app.use(router.routes());
 
 app.listen(3000);
