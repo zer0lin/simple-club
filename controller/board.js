423b5e96dd41a95c54b6ab840f7746e2dcb20e91
huyan <huyan00@hotmail.com>
2018-01-03 15:36:38 +0800

    add board ui and on add create_board ui

--- controller/board.js
+++ controller/board.js
@@ -1,3 +1,7 @@
 exports.show = async (ctx, next) => {
   await ctx.render('board');
 }
+
+exports.show_create = async (ctx, next) => {
+  await ctx.render('create_board');
+}
--- route/index.js
+++ route/index.js
@@ -11,6 +11,10 @@ router.get('/', async (ctx, next) => {
 
 // 板块页
 router.get('/board', controller.board.show);
+// 创建板块页
+router.get('/board/create', controller.board.show_create);
+// 创建板块请求
+router.post('/board/create');
 
 // 注册页
 router.get('/signup', auth.checkNotLogin, controller.user.show_signup)
@@ -22,5 +26,7 @@ router.get('/signout', auth.checkLogin, controller.user.signout);
 router.get('/signin', auth.checkNotLogin, controller.user.show_signin);
 // 登陆请求
 router.post('/signin', auth.checkNotLogin, controller.user.signin);
+
+
 
 module.exports = router;
