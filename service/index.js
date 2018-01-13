42576d26ad2e8c0564f0175eb0acc2685b45fe92
huyan <huyan00@hotmail.com>
2018-01-13 16:36:19 +0800

    add user info on main page

--- service/index.js
+++ service/index.js
@@ -1,6 +1,5 @@
 // api分发器
 exports.user = require('./user');
-exports.admin = require('./admin');
 exports.post = require('./post');
 exports.comment = require('./comment');
 exports.board = require('./board');
